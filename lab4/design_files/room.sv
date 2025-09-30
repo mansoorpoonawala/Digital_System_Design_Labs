4bf91570ff1464cd139ae1aab7adc02d161a34aa
mansoorpoonawala <mmp291@scarletmail.rutgers.edu>
2025-09-30 13:21:20 -0400

    Update room.sv

--- lab4/design_files/room.sv
+++ lab4/design_files/room.sv
@@ -2,13 +2,13 @@ module room (input logic clk, n, s, e, w, v, reset, output logic s6, win, s5, d,
 
 logic s0_next, s1_next, s2_next, s3_next, s4_next, s5_next, s6_next;
 
-assign s0_next = reset || (s1 && win && !reset);
-assign s1_next = (s0 && e && !reset) || (s2 && n && !reset);
-assign s2_next = (s1 && s && !reset) || (s3 && e && !reset);
-assign s3_next = (s2 && w && !reset);
-assign s4_next = (s2 && s && e && !reset);
-assign s5_next = (s4 && !v && reset);
-assign s6_next = (s4 && v && !reset);
+assign s0_next = reset || (s0 && !(e)) || (s1 && win);
+assign s1_next = (s0 && e && ~reset) || (s2 && n && ~reset) || (s1 && ~(w || s));
+assign s2_next = (s1 && s && ~reset) || (s3 && e && ~reset) || (s2 && ~(n || w || e));
+assign s3_next = (s2 && w && ~reset) || (s3 && ~(e));
+assign s4_next = (s2 && s && e && ~reset);
+assign s5_next = (s4 && ~v && ~reset);
+assign s6_next = (s4 && v && ~reset);
 assign sw = s3;
 assign d = s5;
 assign win = s6;
@@ -20,5 +20,4 @@ d_ff s3_flop (.d(s3_next), .clk(clk), .q(s3));
 d_ff s4_flop (.d(s4_next), .clk(clk), .q(s4));
 d_ff s5_flop (.d(s5_next), .clk(clk), .q(s5));
 d_ff s6_flop (.d(s6_next), .clk(clk), .q(s6));
-
 endmodule
